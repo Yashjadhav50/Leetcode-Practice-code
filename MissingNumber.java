a242ec4afde27f64817607f1e1789e1dbe9820b2
YASH JADHAV <yashjadhav654321@gmail.com>
2025-12-10 00:57:43 +0530

    ninth code

--- MissingNumber.java
+++ MissingNumber.java
@@ -9,7 +9,7 @@ Output: 2
 
 Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
  2 is the missing number in the range since it does not appear in nums.*/
- 
+
 class Solution {
     public int missingNumber(int[] nums) {
         int range = nums.length;
