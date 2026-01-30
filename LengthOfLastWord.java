137db4d182393f47e92fe62eb5e7db4f958aabcd
YASH JADHAV <yashjadhav654321@gmail.com>
2026-01-30 18:42:51 +0530

    45 code

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -3,9 +3,9 @@ class Solution {
         int count = 0;
 
         for(int i = s.length()-1; i>=0; i--){
-
-            char ch = s.charAt(i);
             
+            char ch = s.charAt(i);
+
             if(ch != ' '){
                 count++;
             }else if(count != 0){
